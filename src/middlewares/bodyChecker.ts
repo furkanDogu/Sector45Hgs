690de64c17f01f84c22ea20d4db5562f0a03ef42
Furkan Doğu <37083315+furkanDogu@users.noreply.github.com>
2019-11-03 01:00:50 +0300

    bodyChecker check reduction

--- src/middlewares/bodyChecker.ts
+++ src/middlewares/bodyChecker.ts
@@ -1,11 +1,7 @@
 import { RequestHandler } from '@appTypes/RequestTypes';
 
 export default (requirements: string[]): RequestHandler<any> => (req, res, next) => {
-    if (
-        !req.body ||
-        requirements.length !== Object.keys(req.body).length ||
-        !requirements.every(requirement => !!req.body[requirement])
-    ) {
+    if (!req.body || !requirements.every(requirement => !!req.body[requirement])) {
         return res.send(`Needed fields are: ${requirements.join(', ')}`);
     }
 
